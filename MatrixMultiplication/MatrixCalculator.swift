4f65e0faf3aa88d4363bef61b34d92a8c34cbfdb
kassergey <kassergeymac@gmail.com>
2019-03-07 01:11:41 +0200

    fuck dispatch group

--- MatrixMultiplication/MatrixCalculator.swift
+++ MatrixMultiplication/MatrixCalculator.swift
@@ -43,32 +43,28 @@ class MatrixCalculator {
         var resultMatrix = Array<Array<Int>>(repeating: Array<Int>(repeating: 0,
                                                                    count: matrixSize),
                                              count: matrixSize)
+        let resultMatrixQueue = DispatchQueue(label: "com.kassergey.matrixCalculator.resultMatrix", attributes: .concurrent)
         for i in 0..<matrixSize {
-            DispatchQueue.global(qos: .userInteractive).async(group: dispatchGroup) {
-                resultMatrix[i] = self.partialMultiplyBlock(withI: i,
-                                                            matrixSize: matrixSize,
-                                                            for: matrix1,
-                                                            for: matrix2)
+            DispatchQueue.global(qos: .userInteractive).async {
+                let local_i = i
+                for j in 0..<matrixSize {
+                    var value = 0
+                    for k in 0..<matrixSize {
+                        value += matrix1[local_i][k] * matrix2[k][j]
+                    }
+                    //this is for fixing crash
+                    resultMatrixQueue.async(group: self.dispatchGroup,
+                                            qos: .userInteractive,
+                                            flags: .barrier,
+                                            execute: {
+                                                resultMatrix[local_i][j] = value
+                    })
+                }
             }
         }
         dispatchGroup.notify(queue: DispatchQueue.main) {
             completion(resultMatrix)
         }
-    }
-    
-    private func partialMultiplyBlock(withI i: Int,
-                                      matrixSize: Int,
-                                      for matrix1:Matrix,
-                                      for matrix2:Matrix) -> [Int] {
-        var resultLine = Array<Int>(repeating: 0, count: matrixSize)
-        for j in 0..<matrixSize {
-            var value = 0
-            for k in 0..<matrixSize {
-                value += matrix1[i][k] * matrix2[k][j]
-            }
-            resultLine[j] = value
-        }
-        return resultLine
     }
     
 }
